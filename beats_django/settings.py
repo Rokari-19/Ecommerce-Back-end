1bb4a4a633b385e06cbf8935c0099e3de15318c0
rokari-19 <oshomahiyelobu1@gmail.com>
2025-01-18 19:11:23 +0100

    trying again

--- beats_django/settings.py
+++ beats_django/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    
 MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
 # celery settings
