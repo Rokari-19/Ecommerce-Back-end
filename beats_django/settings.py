105a9015e6f2f2fb6e84a209828f5614dc29aea5
rokari-19 <oshomahiyelobu1@gmail.com>
2025-01-25 02:41:43 +0100

    debug to true

--- beats_django/settings.py
+++ beats_django/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 '127.0.0.1', 'localhost', '[::1]'
