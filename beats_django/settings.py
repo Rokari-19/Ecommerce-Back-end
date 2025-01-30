0059ae2af20201fd0d4cfae996b53b4eb5281c8e
rokari-19 <oshomahiyelobu1@gmail.com>
2025-01-30 22:59:12 +0100

    switched to email authentication

--- beats_django/settings.py
+++ beats_django/settings.py
@@ -54,6 +54,10 @@ INSTALLED_APPS = [
 
 # AUTH_USER_MODEL = []
 
+DJOSER = {
+    'LOGIN_FIELD': 'email'
+}
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:5173"
@@ -96,19 +100,19 @@ WSGI_APPLICATION = 'beats_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default' : dj_database_url.config(
-#         default='postgresql://user:XfdsNtuLtNpOC7qXXhpoI12uJceSpX4I@dpg-cu5nmttsvqrc738abtug-a.oregon-postgres.render.com/postgres1_12dm',
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.config(
+        default='postgresql://user:XfdsNtuLtNpOC7qXXhpoI12uJceSpX4I@dpg-cu5nmttsvqrc738abtug-a.oregon-postgres.render.com/postgres1_12dm',
+        conn_max_age=600
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # fori, set the db credentials based on however you want it to be. 
 # i already set up the connection on my system so it should be good to go
 
@@ -133,7 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
